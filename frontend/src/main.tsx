51e853483c5aea56d58f534c7a9fe3862031277a
Alemax067 <2657236382@qq.com>
2026-05-11 23:03:58 +0800

    init

---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <main>
+      <h1>Paper Claw</h1>
+      <p>Frontend is running.</p>
+    </main>
+  </React.StrictMode>,
+);
